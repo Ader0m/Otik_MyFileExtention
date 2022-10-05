5fba67a621cf6355ba775fbbd6901874b2556a44
n7arti <n7arti@mail.ru>
2022-10-05 23:42:06 +0300

    4

--- Otik_MyFileExtention/Decoder.cs
+++ Otik_MyFileExtention/Decoder.cs
@@ -32,34 +32,43 @@ namespace Otik_MyFileExtention
             file = new byte[fileStreamInput.Length];
 
             fileStreamInput.Read(file);
-            int i;
-            for (i = 0; i < 4; i++)
-                h.Signature[i] = file[i];
-            Console.WriteLine(i);
-            h.FileOrDirectory = Convert.ToBoolean(file[i]);
-            h.Name = Encoding.UTF8.GetString(file,i+1,i+7);
-            h.Version = BitConverter.ToInt32(file, 13);
-            h.Arhive = BitConverter.ToInt32(file, 17);
-            h.Protect = BitConverter.ToInt32(file, 17);
+            int byt = 3;
+            for (int i = byt; i < (byt+4); i++)
+                h.Signature[i-byt] = file[i];
+            byt += 5;
+            Console.WriteLine(byt);
+            h.FileOrDirectory = Convert.ToBoolean(file[byt]);
+            byt += 2;
+            h.Name = Encoding.UTF8.GetString(file,byt,byt+7);
+            byt += 8;
+            h.Version = BitConverter.ToInt32(file, byt);
+            byt += 5;
+            h.Arhive = BitConverter.ToInt32(file, byt);
+            byt += 5;
+            h.Protect = BitConverter.ToInt32(file, byt);
 
-
-            Console.WriteLine("bool "+h.Name);
-            string s = Encoding.UTF8.GetString(file);
+            Console.WriteLine("Sygnature " + Encoding.UTF8.GetString(h.Signature));
+            Console.WriteLine("bool "+h.FileOrDirectory);
+            Console.WriteLine("Name " + h.Name);
+            Console.WriteLine("Version " + h.Version);
+            Console.WriteLine("Arhive " + h.Arhive);
+            Console.WriteLine("Protect " + h.Protect);
+            //string s = Encoding.UTF8.GetString(file);
             //s = "{\n{\na\nb\n}\n{\nsss\n}\n}";
             //Console.WriteLine(s);
-            s = s.Substring(s.IndexOf("{") + 2, s.LastIndexOf('}') - (s.IndexOf("{") + 3));
-            string header = s.Substring(s.IndexOf('{') + 2, s.IndexOf('}', s.IndexOf('{')) - (s.IndexOf('{') + 3));
-            string[] heeeerd = header.Split("\n");
+            //s = s.Substring(s.IndexOf("{") + 2, s.LastIndexOf('}') - (s.IndexOf("{") + 3));
+            //string header = s.Substring(s.IndexOf('{') + 2, s.IndexOf('}', s.IndexOf('{')) - (s.IndexOf('{') + 3));
+            //string[] heeeerd = header.Split("\n");
 
-            h.Version = Convert.ToInt32(heeeerd[3]);
-            h.Arhive = Convert.ToInt32(heeeerd[4]);
-            h.Protect = Convert.ToInt32(heeeerd[5]);
-            h.StartInfoByte = Convert.ToInt32(heeeerd[6]);
+            //h.Version = Convert.ToInt32(heeeerd[3]);
+            //h.Arhive = Convert.ToInt32(heeeerd[4]);
+            //h.Protect = Convert.ToInt32(heeeerd[5]);
+            //h.StartInfoByte = Convert.ToInt32(heeeerd[6]);
 
-            s = s.Substring(s.IndexOf('}') + 2);
-            string info = s.Substring(s.IndexOf('{') + 2, s.IndexOf('}', s.IndexOf('{')) - (s.IndexOf('{') + 3));
-            Console.WriteLine("header\n" + header);
-            Console.WriteLine("info\n" + info);
+            //s = s.Substring(s.IndexOf('}') + 2);
+            //string info = s.Substring(s.IndexOf('{') + 2, s.IndexOf('}', s.IndexOf('{')) - (s.IndexOf('{') + 3));
+            //Console.WriteLine("header\n" + header);
+            //Console.WriteLine("info\n" + info);
 
 
         }
