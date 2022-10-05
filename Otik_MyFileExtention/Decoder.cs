b43ba871d3c36fe2d418a9b957b531ce962b30e1
n7arti <n7arti@mail.ru>
2022-10-06 01:21:01 +0300

    Сделала декодер. Нужно добавить инфо

--- Otik_MyFileExtention/Decoder.cs
+++ Otik_MyFileExtention/Decoder.cs
@@ -30,47 +30,69 @@ namespace Otik_MyFileExtention
             Storage.IvaExtentionHeader h = new Storage.IvaExtentionHeader();
             FileStream fileStreamInput = File.OpenRead(Storage.NameFile);
             file = new byte[fileStreamInput.Length];
-
             fileStreamInput.Read(file);
-            int byt = 3;
-            for (int i = byt; i < (byt+4); i++)
-                h.Signature[i-byt] = file[i];
-            byt += 5;
-            Console.WriteLine(byt);
-            h.FileOrDirectory = Convert.ToBoolean(file[byt]);
-            byt += 2;
-            h.Name = Encoding.UTF8.GetString(file,byt,byt+7);
-            byt += 8;
-            h.Version = BitConverter.ToInt32(file, byt);
-            byt += 5;
-            h.Arhive = BitConverter.ToInt32(file, byt);
-            byt += 5;
-            h.Protect = BitConverter.ToInt32(file, byt);
+            for (int byt = 0; byt < file.Length; byt++)
+            {
+                byt += 3;
+                for (int i = byt; i < (byt + 4); i++)
+                    h.Signature[i - byt] = file[i];
+                byt += 5;
 
-            Console.WriteLine("Sygnature " + Encoding.UTF8.GetString(h.Signature));
-            Console.WriteLine("bool "+h.FileOrDirectory);
-            Console.WriteLine("Name " + h.Name);
-            Console.WriteLine("Version " + h.Version);
-            Console.WriteLine("Arhive " + h.Arhive);
-            Console.WriteLine("Protect " + h.Protect);
-            //string s = Encoding.UTF8.GetString(file);
-            //s = "{\n{\na\nb\n}\n{\nsss\n}\n}";
-            //Console.WriteLine(s);
-            //s = s.Substring(s.IndexOf("{") + 2, s.LastIndexOf('}') - (s.IndexOf("{") + 3));
-            //string header = s.Substring(s.IndexOf('{') + 2, s.IndexOf('}', s.IndexOf('{')) - (s.IndexOf('{') + 3));
-            //string[] heeeerd = header.Split("\n");
+                h.FileOrDirectory = Convert.ToBoolean(file[byt]);
+                byt += 2;
 
-            //h.Version = Convert.ToInt32(heeeerd[3]);
-            //h.Arhive = Convert.ToInt32(heeeerd[4]);
-            //h.Protect = Convert.ToInt32(heeeerd[5]);
-            //h.StartInfoByte = Convert.ToInt32(heeeerd[6]);
+                int count = 0;
+                while (file[byt + count] != 10)
+                    count++;
+                h.Name = Encoding.UTF8.GetString(file, byt, count);
+                byt += count + 1;
 
-            //s = s.Substring(s.IndexOf('}') + 2);
-            //string info = s.Substring(s.IndexOf('{') + 2, s.IndexOf('}', s.IndexOf('{')) - (s.IndexOf('{') + 3));
-            //Console.WriteLine("header\n" + header);
-            //Console.WriteLine("info\n" + info);
+                count = 0;
+                while (file[byt + count] != 10)
+                    count++;
+                h.Version = Convert.ToInt32(Encoding.UTF8.GetString(file, byt, count));
+                byt += count + 1;
 
+                count = 0;
+                while (file[byt + count] != 10)
+                    count++;
+                h.Arhive = Convert.ToInt32(Encoding.UTF8.GetString(file, byt, count));
+                byt += count + 1;
 
+                count = 0;
+                while (file[byt + count] != 10)
+                    count++;
+                h.Protect = Convert.ToInt32(Encoding.UTF8.GetString(file, byt, count));
+                byt += count + 1;
+
+                count = 0;
+                while (file[byt + count] != 10)
+                    count++;
+                h.StartInfoByte = Convert.ToInt32(Encoding.UTF8.GetString(file, byt, count));
+
+                Console.WriteLine("Sygnature " + Encoding.UTF8.GetString(h.Signature));
+                Console.WriteLine("bool " + h.FileOrDirectory);
+                Console.WriteLine("Name " + h.Name);
+                Console.WriteLine("Version " + h.Version);
+                Console.WriteLine("Arhive " + h.Arhive);
+                Console.WriteLine("Protect " + h.Protect);
+                Console.WriteLine("StartInfoByte " + h.StartInfoByte);
+                Console.WriteLine("INFO ");
+                while (Encoding.UTF8.GetString(file, byt, 1) != "{")
+                {
+                    byt++;
+                }
+                byt += 2;
+                count = 0;
+                while (file[byt + count] != 10)
+                    count++;
+                Console.WriteLine(Encoding.UTF8.GetString(file, byt, count));
+                byt += count + 1;
+                if (h.FileOrDirectory)
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\" + h.Name);
+                else
+                    File.Create(Directory.GetCurrentDirectory() + @"\" + h.Name);
+            }
         }
     }
 }
