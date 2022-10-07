725673620bce1e31793658db280c38b988e76077
K1SELKA@yandex.ru <K1SELKA@yandex.ru>
2022-10-07 13:52:47 +0300

    prepare SymbolFrequency structure

---
+++ Otik_MyFileExtention/SymbolFrequency/SymbolDialog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otik_MyFileExtention.SymbolFrequency
+{
+    internal class SymbolDialog
+    {
+        private ISymbolTask _task;
+
+        public SymbolDialog(ISymbolTask task)
+        {
+            _task = task;
+        }
+
+        public void SymbolDialogStart()
+        {
+            SymbolDialogMenu();
+        }
+
+        private void SymbolDialogMenu()
+        {
+
+        }
+    }
+}
